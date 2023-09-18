5db802b27c31b5801824e98ec092b588630137a3
Hakan TÜZ <hakantr02@gmail.com>
2023-09-18 14:08:19 +0300

    .

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MukemmelSayi {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int toplam = 0;
+
+        System.out.print("Lutfen bir sayi giriniz: ");
+        int sayi = scan.nextInt();
+
+        for(int i = 1; i < sayi; i++)
+        {
+            if(sayi % i == 0) {
+                toplam += i;
+            }
+        }
+        if(sayi == toplam) {
+            System.out.println(sayi + " Mükemmel bir sayidir");
+        }
+        else {
+            System.out.println(sayi + " Mükemmel bir sayi degildir.");
+        }
+    }
+}
+
